d8ef90ffce3269f46cc55c8e70c70c9ed5296277
ViViVidam <1325507519@qq.com>
2024-04-07 19:54:05 -0700

    change to OMP

---
+++ NPB3.4.2/NPB3.4-OMP/compile_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+class="C"
+targets=(bt cg dt ep ft is lu mg sp ua)
+for i in "${targets[@]}"
+do
+   make "$i" CLASS="$class"
+   # or do whatever with individual element of the array
+done
