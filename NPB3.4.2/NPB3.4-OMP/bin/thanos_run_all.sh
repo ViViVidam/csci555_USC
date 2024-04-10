c99438d6b58bbb1e27ecca91161bb3359da0d971
ViViVidam <1325507519@qq.com>
2024-04-09 17:06:00 -0700

    fix thanos run

--- NPB3.4.2/NPB3.4-OMP/bin/thanos_run_all.sh
+++ NPB3.4.2/NPB3.4-OMP/bin/thanos_run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ../../bin
-for f in NPB3.4.2/NPB3.4.2-OMP/*.x; do
+cd ../../../build
+for f in ../NPB3.4.2/NPB3.4.2-OMP/bin/*.x; do
   ./thanos -S 2 $f > "$f.1_output"
   ./thanos -S 2 $f > "$f.2_output"
   ./thanos -S 2 $f > "$f.3_output"
