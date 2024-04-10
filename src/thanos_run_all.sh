9e948d451053a39b06f62ec51a001943994a2d20
ViViVidam <1325507519@qq.com>
2024-04-09 17:31:20 -0700

    fix thanos run

--- src/thanos_run_all.sh
+++ src/thanos_run_all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd ../../../build
-for f in ../NPB3.4.2/NPB3.4.2-OMP/bin/*.x; do
+for f in ../NPB3.4.2/NPB3.4-OMP/bin/*.x; do
   ./thanos -S 2 $f > "$f.1_output"
   ./thanos -S 2 $f > "$f.2_output"
   ./thanos -S 2 $f > "$f.3_output"
