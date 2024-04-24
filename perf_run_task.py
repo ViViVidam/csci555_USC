f27af5106796f2c1be2365f7b6c9843e4ccb818f
ViViVidam <1325507519@qq.com>
2024-04-23 19:19:25 -0700

    fix run

--- perf_run_task.py
+++ perf_run_task.py
@@ -30,7 +30,7 @@ def RunTask(run_seq: bool) -> None:
                             core = splits[0]
                             n = splits[3]
                             count = splits[2]
-                            print(splits)
+                            print(line,splits)
                             if n == "cache-misses":
                                 v_counts[targets.index(perf_out[:2])] += float(count)
                             elif n == "cache-references":
